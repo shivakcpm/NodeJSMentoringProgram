916ddb8ac7e3fa3494da2f9ad0e6f20748a941b9
Shivakrishna.Kongara <shivakrishna.kongara@thomsonreuters.com>
2019-11-26 12:42:00 +0530

    task1 changes

--- src/app/task1.js
+++ src/app/task1.js
@@ -1,14 +1,10 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
 
-   function promptQuestion(){
-    readline.question('enter input : ', async function (result) {
-     await readline.write('\033[1A' + result.split("").reverse().join(""));
-     // readline.close();
-        promptQuestion();
-    });
-  
-}
-promptQuestion();
+  process.stdin.on("data",(result)=> {
+    process.stdout.write(
+       result.toString()
+         .split("")
+         .reverse()
+         .join("")+"\n"
+   );
+ });
+
